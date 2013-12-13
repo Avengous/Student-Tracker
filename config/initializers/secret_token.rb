fd3d9a627a62e150d50a426ede91dab7749add36
Avengous <xxstryderxx@hotmail.com>
2013-12-13 12:26:46 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudentTracker::Application.config.secret_key_base = '71a719b8c4eaa1a613294ecf3448ff420a47c6e03505ba124ac74581d68a076039ca490a9cc6c83142b36533ec89f1fad06fb87e34af8dc54a93ad862c0efb11'
