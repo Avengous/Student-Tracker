86501602fbdb0fb290bbd505bcf7f7c0a0cdf675
Giovanni Schuoler <Avengous@.(none)>
2014-01-02 12:36:19 -0800

    Added some things to user views.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
-  def index
-    @user = User.all
-  end
   
-  def new
+  def index
+    @title = "Users"
+    @user = User.all
   end
   
   def show
     @user = User.find(params[:id])
+    @title = "#{@user.firstname} #{@user.lastname}"
   end
+  
+  def new
+    @title = "New User"
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 StudentTracker::Application.routes.draw do
   get "users/new"
+  get "users/show/:id" => 'users#show'
   get "users" => 'users#index'
   root 'pages#home'
 end
