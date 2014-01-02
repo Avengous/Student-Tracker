2146718ced1d047abce2d137870e0da684775e74
Giovanni Schuoler <Avengous@.(none)>
2014-01-02 10:36:18 -0800

    Added encrypted password migration to users table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :firstname, :lastname, :email
+  attr_accessor   :password
+  attr_accessible :firstname, :lastname, :email, :password, :password_confirmation
   
   email_format = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
   
@@ -14,4 +15,8 @@ class User < ActiveRecord::Base
                     :length => { :maximum => 50},
                     :with => email_format,
                     :uniqueness => { :case_sensitive => false }
+                    
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..20 }
 end
