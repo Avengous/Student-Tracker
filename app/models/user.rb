27cdb62a2e18a1fa454f563108bfac5473a32e0a
Giovanni Schuoler <Avengous@.(none)>
2014-01-02 11:02:35 -0800

    Completed password authentication in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,38 @@ class User < ActiveRecord::Base
   validates :password, :presence => true,
                        :confirmation => true,
                        :length => { :within => 6..20 }
+                       
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  class << self
+    def authenticate(email, submitted_password)
+      user = find_by_email(email)
+      return nil if user.nil?
+      return user if user.has_password?(submitted_password)
+    end
+  end
+  
+  private 
+  
+    def encrypt_password
+      self.salt = create_salt if new_record?
+      self.encrypted_password = encrypt(password)  
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def create_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+    
 end
