4e7aaaa6b1496be29d1278be894e6acc26dfe529
Giovanni Schuoler <Avengous@.(none)>
2014-01-06 09:09:13 -0800

    I don't remember what I did...

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ StudentTracker::Application.routes.draw do
   resources :users
   resources :sessions,      only: [:new, :create, :destroy]
     
-  root 'pages#home'
+  root  'pages#home'
   
   match '/signin' => 'sessions#new', via: [:get, :post]
   match '/signout' => 'sessions#destroy', via: [:get, :post]
