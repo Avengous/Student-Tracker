44691086667e6b3c900ddc9aa37e9cc58dfbe827
Giovanni Schuoler <Avengous@.(none)>
2014-01-07 12:14:38 -0800

    Added Student and Laptop validations.

--- app/models/laptop.rb
+++ app/models/laptop.rb
@@ -1,2 +1,9 @@
 class Laptop < ActiveRecord::Base
+  attr_accessible :itemname, :scancode, :available
+  
+  validates :itemname, :presence => true,
+                        :length => { :maximum => 20}
+                        
+  validates :scancode,  :presence => true,
+                        :length => { :maximum => 20}
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,16 @@
 class Student < ActiveRecord::Base
+  attr_accessible :firstname, :lastname, :email
+  
+  before_save { self.email = email.downcase }
+  
+  validates :firstname, :presence => true,
+                        :length => { :maximum => 20}
+                        
+  validates :lastname,  :presence => true,
+                        :length => { :maximum => 20}
+  
+  VALID_EMAIL_REGEX = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX, multiline: true },
+                    uniqueness: { case_sensitive: false }
 end
