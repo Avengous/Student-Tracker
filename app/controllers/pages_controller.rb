e9a6748dfe582f301e9290287142015d1d4be3df
Giovanni Schuoler <Avengous@.(none)>
2014-01-23 14:27:24 -0800

    Added Master Delete.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :admin_user,     only: [:index]
+  
   def home
     @title = "Home"
     redirect_to user_path(current_user.id)
@@ -7,4 +9,30 @@ class PagesController < ApplicationController
   def feedback
     @title = 'Feedback'
   end
+  
+  def database
+    @title = 'Database'
+  end
+  
+  def delete_all_students
+    Laptop.update_all( "available = 'true'" )
+    Student.destroy_all
+    redirect_to database_path
+  end
+  
+  def delete_all_laptops
+    Laptop.destroy_all
+    redirect_to database_path
+  end
+  
+  def delete_all_tickets
+    Ticket.destroy_all
+    redirect_to database_path
+  end
+  
+  private
+  
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ StudentTracker::Application.routes.draw do
     
   root  'pages#home'
   
-  match '/resolved_tickets' => 'tickets#get_resolved', via: [:get]
-  
+  match '/resolved_tickets' => 'tickets#get_resolved', via: [:get]  
   match '/checkin' => 'transactions#checkin', via: [:get, :post]
+  
   match '/add_multiple_users' => 'users#add_multiple_users', via: [:get, :post, :put]
   match '/add_multiple_students' => 'students#add_multiple_students', via: [:get, :post]
   match '/add_multiple_laptops' => 'laptops#add_multiple_laptops', via: [:get, :post, :put]
@@ -22,5 +22,15 @@ StudentTracker::Application.routes.draw do
   match '/signout' => 'sessions#destroy', via: [:get, :post]
   
   match '/turnin/:id' => 'transactions#turn_in', via: [:get, :post, :put]
+  
   get 'feedback' => 'pages#feedback'
+  
+  get 'database' => 'pages#database'
+  
+  resources :pages do
+    put :delete_all_students, on: :collection
+    put :delete_all_laptops, on: :collection
+    put :delete_all_tickets, on: :collection
+  end
+  
 end
